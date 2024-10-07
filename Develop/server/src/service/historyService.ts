f44e9084a3cc46d4880a3697a3fa6822137f6ac1
Magsmags600 <margaretjenkins@gmail.com>
2024-10-06 17:04:32 -0700

    fixed main.ts

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,18 +1,3 @@
-// // TODO: Define a City class with name and id properties
-
-// // TODO: Complete the HistoryService class
-// class HistoryService {
-//   // TODO: Define a read method that reads from the searchHistory.json file
-//   // private async read() {}
-//   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-//   // private async write(cities: City[]) {}
-//   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-//   // async getCities() {}
-//   // TODO Define an addCity method that adds a city to the searchHistory.json file
-//   // async addCity(city: string) {}
-//   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-//   // async removeCity(id: string) {}
-// }
 
 // export default new HistoryService();
 //
@@ -28,9 +13,9 @@ class HistoryService {
   private filePath = 'searchHistory.json';
 
   // Define a read method that reads from the searchHistory.json file
-  private async read(): Promise<City[]> {
+  private async read() {
     const data = await fs.readFile(this.filePath, 'utf-8');
-    return JSON.parse(data) as City[];
+    return data;
   }
 
   // Define a write method that writes the updated cities array to the searchHistory.json file
@@ -39,8 +24,17 @@ class HistoryService {
   }
 
   // Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  async getCities(): Promise<City[]> {
-    return this.read();
+  async getCities() {
+    return await this.read().then(data => {
+      let parsedCities: City[]; 
+      try { 
+        parsedCities = [].concat(JSON.parse(data)); 
+        
+      } catch (error) { parsedCities = []; }
+      return parsedCities;
+        
+      
+    });
   }
 
   // Define an addCity method that adds a city to the searchHistory.json file
